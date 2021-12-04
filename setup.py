bf65eea1e884c666847d5c887eee095c5fbbec9c
UAP Paul <uappaul@gmail.com>
2021-12-03 17:18:38 -0800

    create package

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(name='mldetector',
+                 version='0.1',
+                 description='ML Detector for simpletracker',
+                 url='#',
+                 author='UAP Paul',
+                 install_requires=['torch', 'torchvision'],
+                 author_email='',
+                 packages=setuptools.find_packages(),
+                 zip_safe=False)
